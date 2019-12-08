681b6e9997ca85c9fb67c2d74d2b76ab3c8fe839
Natalie Soberano <nataliesoberano@Natalies-MacBook-Pro-4.local>
2019-12-08 17:15:27 -0500

    more changes

--- utils/sql.js
+++ utils/sql.js
@@ -2,13 +2,15 @@
 const config = require("../config");
 const sql = require('mysql');
 
-const connect = sql.createConnection({
+const connect = sql.createPool({
     host: config.host,
     port: config.port,
-    user: config.uname, 
-    password: config.pword, 
-    database: config.dbase
-
-})
+    uname: config.uname, 
+    pword: config.pword, 
+    dbase: config.dbase,
+    connectionLimit : 20,
+    queueLimit : 100,
+    waitForConnections : true
+});
 
 module.exports = connect;
